032212a390677b10a0a75f609c7fa7a658f5e5a0
longwanheng <lwh65832814@163.com>
2019-11-24 17:04:23 +0800

    第一课：      HelloWorld

---
+++ src/test/java/learn/Test.java
@@ -0,0 +1,11 @@
+package learn;
+
+/**
+ * Test
+ *
+ * @author : 龙万恒
+ * @version :v1.0
+ * @date : 2019-11-24 15:51
+ */
+public class Test {
+}
---
+++ src/test/java/learn/day01/HelloWorldTeach.java
@@ -0,0 +1,63 @@
+package learn.day01;
+
+/**
+ * HelloWorldTeach
+ *
+ * @author : 龙万恒
+ * @version :v1.0
+ * @date : 2019-11-24 15:51
+ */
+public class HelloWorldTeach {
+    /*
+     * 1.请用记事本新建一个类,类名为HelloWorld,并手写一个HelloWorld
+     */
+
+
+    /*
+    public class HelloWorld {
+
+        public static void main(String[] args) {
+            System.out.println("Hello world");
+        }
+    }
+    * 关键字说明
+    *
+    * public：代表公共的、(其他包可以访问)
+    * class: 代表声明一个类
+    *
+    * static 代表静态的
+    * void 代表没有返回值的
+    *
+    * String[] 代表一个String类型的数组
+    * ===========================================================
+    * HelloWorld：
+    *   作为类名，一定要遵循命名规范
+    *   类名的命名规范为:一般为具有代表这个类作用的英文单词组合，每个单词首字母大写,
+    *   如:User、HelloWorld、OnlineUser 避免出现类名中使用特殊符号、空格、数字等
+    * main
+    *   代表的是一个方法名，
+    *   public static void main(String[] args)
+    *  代表的是 一个公共的，静态的，没有返回值的，入参为一个String[]类型数组的，方法名为main的方法(也叫函数)
+    *  main方法是java程序的执行入口，也就是说 要执行一个类里面的逻辑 就是从main方法开始执行的
+    *
+    * 方法(也叫函数c、c++、前端开发人员喜欢叫函数，java开发者喜欢叫方法)的命名规范：
+    *   驼峰命名 跟类名不同的是 他要求除第一个单词的首字母小写外、从第二个单词开始首字母都要大写
+    *
+    * 变量命名:
+    *       int number = 1;
+    *       boolean flag = true;
+    *      变量命名要求跟方法名基本相同
+    * 命名规范的意义：
+    *       首先，拿类名来说 比如User，一看到首字母大写，首先想到的是 这是一个类，再看类名
+    *       就能知道 User是一个用户类，跟用户相关
+    *       如果命名随意，如类名为:Abcdefg,可能当时知道这个类是什么类，过一段时间就不明所以了。
+    *       这样做的好处是，我可以清晰的看出这是一个类,并且知道这个类的意义。
+    *
+    *       同理 在看到 getUserInfo()的时候，首先就能明白 这是一个方法，这个方法的作用是获取用户信息(方法里面的逻辑先不管)
+    *
+    * *重要的一点！！！*
+    *   不论是类名、方法名、变量名 都不能是java中的关键字
+    *   比如 class static{} void() int public = 1;
+    */
+}
+
