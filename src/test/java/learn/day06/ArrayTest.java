365788a8dbbb5d644e22e6dd2fc6492c8444a78e
longwanheng <lwh65832814@163.com>
2019-12-06 23:31:07 +0800

    test done

---
+++ src/test/java/learn/day06/ArrayTest.java
@@ -0,0 +1,24 @@
+package learn.day06;
+
+/**
+ * ArrayTest
+ *
+ * @author : 龙万恒
+ * @version :v1.0
+ * @date : 2019-12-06 22:54
+ */
+public class ArrayTest {
+
+    public static void main(String[] args) {
+        // byte short int long double char float boolean
+        //数字：int long short  byte
+        //byte -128 - 127
+        //顺序从小到大 byte short int long
+        //浮点数(小数)
+        // float 单精度 double双精度
+        //字符:char，数字 、英文字母、空格、符号、汉字都是一个字符
+        char a = '数';
+        //char看成一个珍珠
+        //ABC
+    }
+}
